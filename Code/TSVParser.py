d82c2d190364ae0b855355c88f15521596723543
Cassandra Loor <the_cas02@hotmail.com>
2014-11-22 13:47:51 +0100

    TSV parser works with new tsv file
    Not sure if the behaviour is what we want yet. Also added new tsv files,
    as we didn't have that yet.

--- Code/TSVParser.py
+++ Code/TSVParser.py
@@ -5,22 +5,36 @@ from pprint import pprint
 
 '''Feature extraction class'''
 
-
 class TSV_Object:
     # Store tsv object
     # One TSV object per tweet
     def __init__(self, tsv_object, data_type, emotion):
-        self.data_type = data_type
-        self.emotion = emotion
+        self.data_type = data_type.lower()
+        self.emotion = emotion.lower()
         self.tsv_object = tsv_object
-        self.text = tsv_object[0]
-        self.hashtags = tsv_object[1]
+        self.timestamp = None
+        self.username = None
+        self.text = None
+        self.hashtags = None
+        print 'tsv_object: ', tsv_object
 
-    # self.emoticons = tsv_object[2]
+        if data_type == "twitter":
+            self.text = tsv_object[0]
+            self.hashtags = tsv_object[1]
+        elif data_type == "ubuntu":
+            self.timestamp = tsv_object[0]
+            self.username = tsv_object[1]
+            self.text = tsv_object[2]
 
     # Get raw text from tsv object
     def get_text(self):
         return self.text
+
+    def get_timestamp(self):
+        return self.timestamp
+
+    def get_username(self):
+        return self.username
 
     # Get emoticons from tsv object
     def get_emoticons(self):
@@ -33,17 +47,20 @@ class TSV_Object:
 
     # Get hashtags from tsv object
     def get_tags(self):
-        tags = []
-        hashtags = self.hashtags.split(',')
-        for tag in hashtags:
-            print 'tag: ', tag
-            tags.append(tag)
-        return tags
-
+        if self.hashtags is not None:
+            tags = []
+            hashtags = self.hashtags.split(',')
+            for tag in hashtags:
+                print 'tag: ', tag
+                tags.append(tag)
+            return tags
+        else:
+            return self.hashtags
 
 class TSV_Getter:
     def __init__(self, filename, verbose=0):
         self.all_tsv_objects = []
+        filename = filename.lower()
         print filename
         if "ubuntu" in filename:
             data_type = "ubuntu"
@@ -68,12 +85,13 @@ class TSV_Getter:
             tsvIn = csv.reader(tsvIn, delimiter='\t')
             for row in tsvIn:
                 # Do not store the first row
-                if (row[0] == 'Text'):
-                    continue
+                #if (row[0] == 'Text'):
+                #    continue
                 # Create tsv object of row (which is one tweet)
                 tsv_obj = TSV_Object(row, data_type, emotion)
                 # Store all tsv objects of this file in this getter class
-                self.all_tsv_objects.append(tsv_obj)
+                #self.all_tsv_objects.append(tsv_obj)
+                print row
 
     def get_all_tsv_objects(self, size=None):
         if size is None:
@@ -82,6 +100,7 @@ class TSV_Getter:
 
 
 if __name__ == "__main__":
-    dataPoints = [[_.text, _.hashtags, ':('] for _ in TSV_Getter('negative.tsv').get_all_tsv_objects()]
-    dataPoints = dataPoints + [[_.text, _.hashtags, ':)'] for _ in TSV_Getter('positive.tsv').get_all_tsv_objects()]
+    dataPoints = [[_.text, _.hashtags, ':('] for _ in TSV_Getter('../Data/Chat Data/2006-04-20-#ubuntu-negative.tsv').get_all_tsv_objects()]
+    print 'going to positive!\n'
+    dataPoints = dataPoints + [[_.text, _.hashtags, ':)'] for _ in TSV_Getter('../Data/Chat Data/2006-04-20-#ubuntu-positive.tsv').get_all_tsv_objects()]
     print len(dataPoints)
