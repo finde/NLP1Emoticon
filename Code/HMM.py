a74000405befaef62ad92f8f40c28fedfe3ea36f
igornishka <iva.gornishka@student.uva.nl>
2014-12-03 00:27:54 +0200

    better HMM

--- Code/HMM.py
+++ Code/HMM.py
@@ -11,24 +11,50 @@ We want the data to look like this at the end:
 class HMM:
     def __init__(self, number_of_clusters , labels):
         
-        self.states = ['start'] + labels
         self.labels = labels
         self.number_of_clusters = number_of_clusters
         
-        self.total_transition_counts = {state: {trans_state: 0 for trans_state in self.states} for state in self.states}     
-        self.transition_probs = {state: {trans_state: 0 for trans_state in self.states} for state in self.states} 
+        self.transition_counts = {state: {trans_state: 0 for trans_state in self.get_states()} for state in self.get_states()}     
+        self.transition_probs = {state: {trans_state: 0 for trans_state in self.get_states()} for state in self.get_states()} 
         
-        self.total_emission_counts = {label: {cluster: 0 for cluster in range(1, number_of_clusters+1)} for label in self.labels}
+        self.emission_counts = {label: {cluster: 0 for cluster in range(1, number_of_clusters+1)} for label in self.labels}
         self.emission_probs = {label: {cluster: 0 for cluster in range(1, number_of_clusters+1)} for label in self.labels}
         
     
-
+    # Labels 
+    def get_labels(self):
+        return self.labels
+        
+    # Labels + the 'start' state!    
+    def get_states(self):
+        return ['start'] + self.get_labels()
+    
+    def get_number_of_clusters(self):
+        return self.number_of_clusters
+    
+    def update_transitions_probs(self):
+        # Be careful not to devide by 0!!
+        for state in self.transition_counts.keys():
+            total = float(sum((self.transition_counts[state]).values()))
+            if total > 0:
+                self.transition_probs[state] = {trans_state: self.transition_counts[state][trans_state]/total 
+                                                    for trans_state in self.get_states()}
+    
+        
+    def update_emission_probs(self):
+        # Be careful not to devide by 0!!
+        for state in self.emission_counts.keys():
+            total = float(sum((self.emission_counts[state]).values()))
+            if total > 0:
+                self.emission_probs[state] = {cluster: self.emission_counts[state][cluster]/total 
+                                                for cluster in range(1, self.number_of_clusters+1)}
+            
     
     def add_data(self, data, labels):
         
         # Initialize the dict for the current transition counts. 
         # Count a transition from start to the first label and from the last label back to start.
-        current_transition_counts = {state: {trans_state: 0 for trans_state in self.states} for state in self.states}
+        current_transition_counts = {state: {trans_state: 0 for trans_state in self.get_states()} for state in self.get_states()}
         current_transition_counts['start'][labels[0]] += 1
         current_transition_counts[labels[len(labels)-1]]['start'] += 1
         
@@ -39,18 +65,12 @@ class HMM:
             current_transition_counts[old_state][new_state] += 1    
         
         # Update the total_transition_counts
-        self.total_transition_counts = {
-                state: {trans_state: self.total_transition_counts[state][trans_state] + current_transition_counts[state][trans_state] 
-                    for trans_state in self.states} for state in self.states}
+        self.transition_counts = {
+                state: {trans_state: self.transition_counts[state][trans_state] + current_transition_counts[state][trans_state] 
+                    for trans_state in self.get_states()} for state in self.get_states()}
         
         # Update the probabilities 
-        # Be careful not to devide by 0!!
-        for state in self.total_transition_counts.keys():
-            print 'state', state
-            total = float(sum((self.total_transition_counts[state]).values()))
-            if total > 0:
-                self.transition_probs[state] = {trans_state: self.total_transition_counts[state][trans_state]/total for trans_state in self.states}
-        
+        self.update_transitions_probs()
         
         # Initialize the dict for the current emission counts:
         current_emission_counts = {label: {cluster: 0 for cluster in range(1, self.number_of_clusters+1)} for label in self.labels}
@@ -62,25 +82,21 @@ class HMM:
             current_emission_counts[labels[i]][data[i]] += 1
         
         # Update the total_emission_counts
-        self.total_emission_counts = {
-                label: {cluster: self.total_emission_counts[label][cluster] + current_emission_counts[label][cluster] 
+        self.emission_counts = {
+                label: {cluster: self.emission_counts[label][cluster] + current_emission_counts[label][cluster] 
                     for cluster in range(1, self.number_of_clusters+1)} for label in self.labels}
         
         
         
         # Update the probabilities
-        # Be careful not to devide by 0!!
-        for state in self.total_emission_counts.keys():
-            print 'state', state
-            total = float(sum((self.total_emission_counts[state]).values()))
-            if total > 0:
-                self.emission_probs[state] = {cluster: self.total_emission_counts[state][cluster]/total for cluster in range(1, self.number_of_clusters+1)}
+        self.update_emission_probs()
         
-        print 'Transition counts:', self.total_transition_counts
+        
+        print 'Transition counts:', self.transition_counts
         print 'Transition probs:', self.transition_probs
-        print 'Emission counts:', self.total_emission_counts
+        print 'Emission counts:', self.emission_counts
         print 'Emission probs:', self.emission_probs
         
         
-model = HMM(15, ['p', 'n'])
-model.add_data([1, 5, 2, 7], ['n', 'n', 'n', 'n'])
+model = HMM(15, ['p', 'n', 'l'])
+model.add_data([1, 5, 2, 7, 4], ['p', 'n', 'l','n', 'n'])
