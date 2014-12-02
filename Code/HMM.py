1c3b945660abb6584f95a760448eb514c2a85d74
igornishka <iva.gornishka@student.uva.nl>
2014-12-02 16:08:59 +0200

    HMM Started

---
+++ Code/HMM.py
@@ -0,0 +1,11 @@
+
+
+class HMM:
+    def __init__(self):
+        self.counts = dict()
+        self.total_count = dict()
+        self.transition_probs = dict()
+        self.emission_probs = dict()
+        
+    
+    
