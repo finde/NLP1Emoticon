18ba4b63b4c374a030b7d4778f62a47a98db6c75
Cassandra <cassandra.loor@student.uva.nl>
2014-12-02 11:27:10 +0100

    added new parser

--- Code/TSVParser.py
+++ Code/TSVParser.py
@@ -1,9 +1,11 @@
 import csv
-from DataPreprocessor import DataPreprocessor
-
+import sys
+import pdb
+import numpy as np
+from pprint import pprint
+from copy import deepcopy
 
 '''Feature extraction class'''
-
 
 class TSV_Object:
     # Store tsv object
@@ -16,21 +18,14 @@ class TSV_Object:
         self.username = None
         self.text = None
         self.hashtags = None
-        # print 'tsv_object: ', tsv_object
 
         if data_type == "twitter":
             self.text = tsv_object[0]
             self.hashtags = tsv_object[1]
-        elif data_type == "twitter2":  # data from hoang
-            preprocessor = DataPreprocessor()
-            self.text, self.hashtags = preprocessor.remove_hashtags(tsv_object[5])
         elif data_type == "ubuntu":
             self.timestamp = tsv_object[0]
             self.username = tsv_object[1]
             self.text = tsv_object[2]
-        else:
-            self.text = tsv_object[0]
-            self.hashtags = tsv_object[1]
 
     # Get raw text from tsv object
     def get_text(self):
@@ -47,7 +42,7 @@ class TSV_Object:
         emoticons = []
         emoticons_array = self.emoticons.split(',')
         for emo in emoticons_array:
-            # print 'emo: ', emo
+            print 'emo: ', emo
             emoticons.append(emo)
         return emoticons
 
@@ -57,68 +52,83 @@ class TSV_Object:
             tags = []
             hashtags = self.hashtags.split(',')
             for tag in hashtags:
-                # print 'tag: ', tag
+                print 'tag: ', tag
                 tags.append(tag)
             return tags
         else:
             return self.hashtags
 
-
 class TSV_Getter:
-    def __init__(self, filename, data_type=None, emotion=None):
+    def __init__(self, filename, verbose=0):
         self.all_tsv_objects = []
-        filename = filename.lower()
-        # print filename
+        self.sorted_tsv_objects  = []
+        filename_copy = filename.lower()
+        print filename
+        if "ubuntu" in filename_copy:
+            data_type = "ubuntu"
+        elif "twitter" in filename_copy:
+            data_type = "twitter"
+        else:
+            data_type = "unknown"
 
-        if data_type is None:
-            if "ubuntu" in filename:
-                data_type = "ubuntu"
-            elif "twitter" in filename:
-                data_type = "twitter"
-            else:
-                data_type = "unknown"
-
-        if emotion is None:
-            if "positive-negative" in filename:
-                emotion = "mixed"
-            elif "positive" in filename:
-                emotion = "positive"
-            elif "negative" in filename:
-                emotion = "negative"
-            elif "neutral" in filename:
-                emotion = "neutral"
-            else:
-                emotion = "unknown"
+        if "positive-negative" in filename_copy:
+            emotion = "mixed"
+        elif "positive" in filename_copy:
+            emotion = "positive"
+        elif "negative" in filename_copy:
+            emotion = "negative"
+        elif "neutral" in filename_copy:
+            emotion = "neutral"
+        else:
+            emotion = "unknown"
 
         # Read TSV file:
         with open(filename, 'rb') as tsvIn:
-
-            # allow to read tsv and csv (default)
-            if ".tsv" in filename:
-                tsvIn = csv.reader(tsvIn, delimiter='\t')
-            else:
-                tsvIn = csv.reader(tsvIn, delimiter=',')
-
+            tsvIn = csv.reader(tsvIn, delimiter='\t')
             for row in tsvIn:
                 # Do not store the first row
-                # if (row[0] == 'Text'):
-                # continue
                 # Create tsv object of row (which is one tweet)
                 tsv_obj = TSV_Object(row, data_type, emotion)
-                # print row
                 # Store all tsv objects of this file in this getter class
                 self.all_tsv_objects.append(tsv_obj)
+                
+        self.sort_tsv_objects()
 
     def get_all_tsv_objects(self, size=None):
         if size is None:
             size = len(self.all_tsv_objects)
         return self.all_tsv_objects[0:size]
-
+    
+    def sort_tsv_objects(self):
+        #sort tsv objects by username
+        
+        # keep track of usernames already checked
+        usernames = []
+        
+        # get length of array
+        length = len(self.all_tsv_objects)
+        # loop through all messages to find username and sort per user
+        for message in range(0, length):
+            username = self.all_tsv_objects[message].get_username()
+            if username not in usernames:
+                usernames.append(username)
+                user_messages = []
+                for i in range(0, length):
+                    if(self.all_tsv_objects[i].get_username() == username):
+                        user_messages.append(self.all_tsv_objects[i])
+                self.sorted_tsv_objects.append(user_messages)
+         
+        #Turned off for testing purposes       
+        for i in range(0, len(self.sorted_tsv_objects)):
+            for sorted in range(0, len(self.sorted_tsv_objects[i])):
+                print self.sorted_tsv_objects[i][sorted].get_username()
 
 if __name__ == "__main__":
-    dataPoints = [[_.text, _.hashtags, ':('] for _ in
-                  TSV_Getter('../Data/Chat Data/2006-04-20-#ubuntu-negative.tsv').get_all_tsv_objects()]
-    # print 'going to positive!\n'
-    dataPoints = dataPoints + [[_.text, _.hashtags, ':)'] for _ in
-                               TSV_Getter('../Data/Chat Data/2006-04-20-#ubuntu-positive.tsv').get_all_tsv_objects()]
-    # print len(dataPoints)
+    tsv_negative = TSV_Getter('../Data/Chat Data/2006-06-01-#ubuntu-negative.tsv').get_all_tsv_objects()
+    #dataPoints = [[_.text, _.hashtags, ':('] for _ in TSV_Getter('../Data/Chat Data/2006-06-01-#ubuntu-negative.tsv').get_all_tsv_objects()]
+    
+    #print dataPoints
+    
+    #print 'going to positive!\n'
+    #dataPoints = dataPoints + [[_.text, _.hashtags, ':)'] for _ in TSV_Getter('../Data/Chat Data/2006-06-01-#ubuntu-positive.tsv').get_all_tsv_objects()]
+    #print len(dataPoints)
