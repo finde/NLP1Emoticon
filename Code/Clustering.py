37219fa00a5956976037cc790156ad1aa011670f
igornishka <iva.gornishka@student.uva.nl>
2014-12-08 10:51:02 +0200

    Updated things to work with getData

--- Code/Clustering.py
+++ Code/Clustering.py
@@ -6,6 +6,7 @@ import DataPoint
 from TSVParser import TSV_Getter
 import scipy.cluster.vq as cluster
 import cPickle
+from getData import GetData
 
 
 # Cluster features, using K-means
@@ -41,69 +42,72 @@ def get_nearest_clusters_matrix(clusters, matrix):
 
 
 if __name__ == "__main__":
-    # load dictionary
-    print('Reading data...')
+        
+    training_percentage = 1
+    
+    '''
+    # Reading
+    '''
+
+    n_per_class = 100
+    
     data_class = [
         ['../Data/Twitter/hc1', 0, ';-)'],
-        ['../Data/Twitter/hc2', 1, ';D'],
-        ['../Data/Twitter/hc3', 2, ';)'],
-        # ['../Data/Twitter/hc4', 3, ';-D'],
-        # ['../Data/Twitter/hc5', 4, ';-P'],
-        # ['../Data/Twitter/hc6', 5, ';P'],
-        # ['../Data/Twitter/hc7', 6, ';-('],
-        # ['../Data/Twitter/hc8', 7, ';('],
-        # ['../Data/Twitter/hc9', 8, ';o'],
-        # ['../Data/Twitter/hc10', 9, ';]'],
-        # ['../Data/Twitter/hc11', 10, '=]'],
-        # ['../Data/Twitter/hc13', 11, ';*'],
-        # ['../Data/Twitter/hc15', 12, ';|'],
-        # ['../Data/Twitter/hc_non', 13, '_non_'],
+        #['../Data/Twitter/hc2', 1, ';D'],
+        #['../Data/Twitter/hc3', 2, ';)'],
+        #['../Data/Twitter/hc4', 3, ';-D'],
+        #['../Data/Twitter/hc5', 4, ';-P'],
+        #['../Data/Twitter/hc6', 5, ';P'],
+        #['../Data/Twitter/hc7', 6, ';-('],
+        ['../Data/Twitter/hc8', 7, ';('],
+        ['../Data/Twitter/hc9', 8, ';o'],
+        #['../Data/Twitter/hc10', 9, ';]'],
+        #['../Data/Twitter/hc11', 10, '=]'],
+        #['../Data/Twitter/hc13', 11, ';*'],
+        #['../Data/Twitter/hc15', 12, ';|'],
+        #['../Data/Twitter/hc_non', 13, '_non_'],
     ]
     
-    # get data points
-    data_points = []
-    amount_data_per_class = 50
-    
-    for c in data_class:
-        # comment line below for balanced data source
-        #amount_data_per_class = None
-        data_points = data_points + [DataPoint.DataPoint(_.text, _.hashtags, c[1]) for _ in
-                                        TSV_Getter(c[0]).get_all_tsv_objects(amount_data_per_class)]
-    
-    print('Feature extraction...')
-    # gather the data points into a whole training data
-    training_data = TrainingData.TrainingData(data_points)
-    
-    
+    #data_class = [
+    #    ['2006-05-27-#ubuntu-negative.tsv', ':('],
+    #    ['2006-05-27-#ubuntu-positive.tsv', ':)']
+    # ]
+
+    # load data from tsv and build data collection
+    selected_features = [
+        "words",
+        "negative_words",
+        "positive_words",
+        "positive_words_hashtags",
+        "negative_words_hashtags",
+        "uppercase_words",
+        "special_punctuation",
+        "adjectives"
+    ]
+
+    dataCollection = GetData(data_class, n_per_class, training_percentage, selected_features)
+
+    # split data collection into training and test data
+    training_data = dataCollection.get_training_data()
+    training_label = np.array(dataCollection.get_training_label())
+ 
+    print('Extracting features...')
+
     # Get the feature matrix of this data
-    # filename = '../Data/Twitter/cache.__' + `len(data_class)` + '-Emo__.p'
-    # if os.path.isfile(filename) and os.access(filename, os.R_OK):
-    # fh = open(filename, "rb")
-    # feat_matrix = cPickle.load(fh)
-    # fh.close()
-    # else:
-    # fh = open(filename, "wb")
-    feat_matrix = np.array(training_data.get_feature_matrix())
-    labels = training_data.get_label_vector()
-    
-    # cPickle.dump(feat_matrix, fh)
-    # fh.close()x
+    print(' extracting train_data')
+    training_features = dataCollection.get_training_feature_matrix()
+
+    number_classes = len(data_class)
     
     number_of_clusters = 50
+    
     # Find cluster centers
-    cluster_centers, assignment = cluster_feature_matrix(feat_matrix, number_of_clusters)
-    nearest_clusters = get_nearest_clusters_matrix(cluster_centers, feat_matrix)
+    cluster_centers, assignment = cluster_feature_matrix(training_features, number_of_clusters)
+    #nearest_clusters = get_nearest_clusters_matrix(cluster_centers, training_features)
     
-    # print nearest clusters. use only for testing purposes!
-    print "nearest clusters: ", nearest_clusters
-    print ' DONE' 
-    print labels
-    
-    
-    # Just checking if the clusters are sort of consistent:
+        # Just checking if the clusters are sort of consistent:
     for i in range(0, number_of_clusters):
         assigned_data = [data_index for data_index in range(0, len(assignment)) if assignment[data_index] == i]
-        print 'Cluster', i, 'contains:', [labels[i] for i in assigned_data] 
-    
-    
+        print 'Cluster', i, 'contains:', [training_label[i] for i in assigned_data] 
+
     
--- Code/HMM.py
+++ Code/HMM.py
@@ -7,7 +7,7 @@ from TSVParser import TSV_Getter
 import scipy.cluster.vq as cluster
 import cPickle
 import Clustering
-
+from getData import GetData
         
                         
 '''''
@@ -110,49 +110,66 @@ class HMM:
 
 
 if __name__ == "__main__":
-    # load dictionary
-    print('Reading data...')
+    training_percentage = 1
+    
+    '''
+    # Reading
+    '''
+
+    n_per_class = 100
+    
     data_class = [
         ['../Data/Twitter/hc1', 0, ';-)'],
         ['../Data/Twitter/hc2', 1, ';D'],
         ['../Data/Twitter/hc3', 2, ';)'],
-        # ['../Data/Twitter/hc4', 3, ';-D'],
-        # ['../Data/Twitter/hc5', 4, ';-P'],
-        # ['../Data/Twitter/hc6', 5, ';P'],
-        # ['../Data/Twitter/hc7', 6, ';-('],
-        # ['../Data/Twitter/hc8', 7, ';('],
-        # ['../Data/Twitter/hc9', 8, ';o'],
-        # ['../Data/Twitter/hc10', 9, ';]'],
-        # ['../Data/Twitter/hc11', 10, '=]'],
-        # ['../Data/Twitter/hc13', 11, ';*'],
-        # ['../Data/Twitter/hc15', 12, ';|'],
-        # ['../Data/Twitter/hc_non', 13, '_non_'],
+        #['../Data/Twitter/hc4', 3, ';-D'],
+        #['../Data/Twitter/hc5', 4, ';-P'],
+        #['../Data/Twitter/hc6', 5, ';P'],
+        #['../Data/Twitter/hc7', 6, ';-('],
+        #['../Data/Twitter/hc8', 7, ';('],
+        #['../Data/Twitter/hc9', 8, ';o'],
+        #['../Data/Twitter/hc10', 9, ';]'],
+        #['../Data/Twitter/hc11', 10, '=]'],
+        #['../Data/Twitter/hc13', 11, ';*'],
+        #['../Data/Twitter/hc15', 12, ';|'],
+        #['../Data/Twitter/hc_non', 13, '_non_'],
     ]
     
-    # get data points
-    data_points = []
-    amount_data_per_class = 500
+    #data_class = [
+    #    ['2006-05-27-#ubuntu-negative.tsv', ':('],
+    #    ['2006-05-27-#ubuntu-positive.tsv', ':)']
+    # ]
+
+    # load data from tsv and build data collection
+    selected_features = [
+        "words",
+        "negative_words",
+        "positive_words",
+        "positive_words_hashtags",
+        "negative_words_hashtags",
+        "uppercase_words",
+        "special_punctuation",
+        "adjectives"
+    ]
+
+    dataCollection = GetData(data_class, n_per_class, training_percentage, selected_features)
+
+    # split data collection into training and test data
+    training_data = dataCollection.get_training_data()
+    training_label = np.array(dataCollection.get_training_label())
+ 
+    print('Extracting features...')
+
+    # Get the feature matrix of this data
+    print(' extracting train_data')
+    training_features = dataCollection.get_training_feature_matrix()
+
+    number_classes = len(data_class)
     
-    for c in data_class:
-        # comment line below for balanced data source
-        #amount_data_per_class = None
-        data_points = data_points + [DataPoint.DataPoint(_.text, _.hashtags, c[1]) for _ in
-                                        TSV_Getter(c[0]).get_all_tsv_objects(amount_data_per_class)]
+    number_of_clusters = 50
     
-    print('Feature extraction...')
-    # gather the data points into a whole training data
-    training_data = TrainingData.TrainingData(data_points)
-    
-    
-    feat_matrix = np.array(training_data.get_feature_matrix())
-    labels = training_data.get_label_vector()
-    
-    
-    number_of_clusters = 15
     # Find cluster centers
-    
-    cluster_centers, assignment = Clustering.cluster_feature_matrix(feat_matrix, number_of_clusters)
-    
+    cluster_centers, assignment = Clustering.cluster_feature_matrix(training_features, number_of_clusters)
     
      
     # Because the clusters are 0-14 and I want them 1:15 :P :P Gonna fix this..   
@@ -162,7 +179,7 @@ if __name__ == "__main__":
     model = HMM(number_of_clusters, data_classes)
     print 'size assinment: ', len(assignment)
     print 'assignment', assignment
-    print 'size labels', len(labels)
-    print 'lables', labels
-    model.add_data(assignment, labels)
+    print 'size labels', len(training_label)
+    print 'lables', training_label
+    model.add_data(assignment, training_label)
  
